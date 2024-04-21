8c815c83402ec9421ac3c1da1a2555e579fdcd4a
Valentino Cueto <valentinocueto2005@gmail.com>
2024-04-21 15:25:23 -0300

    ejercicio resuelto

--- beers-proyect/src/components/beers/Beers.jsx
+++ beers-proyect/src/components/beers/Beers.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button, Card, Col, Form, Row } from "react-bootstrap";
 
 const beersList = [
   {
@@ -67,6 +68,39 @@ const beersList = [
 ];
 
 const Beers = () => {
+  const [name, setName] = useState("");
+  const [style, setStyle] = useState("");
+  const [price, setPrice] = useState("");
+  const [available, setAvailable] = useState("");
+
+  const changeNameHandler = (event) => {
+    setName(event.target.value);
+  };
+  const changeStyleHandler = (event) => {
+    setStyle(event.target.value);
+  };
+  const changePriceHandler = (event) => {
+    setPrice(event.target.value);
+  };
+  const changeAvailable = (event) => {
+    setAvailable(event.target.value);
+  };
+
+  const submitBeerHandler = (event) => {
+    event.preventDefault();
+    const newBeer = {
+      name,
+      style,
+      price,
+      available,
+    };
+    console.log(newBeer);
+    setName("");
+    setStyle("");
+    setPrice("");
+    setAvailable("");
+  };
+
   const [priceDollar, setPriceDollar] = useState(1000);
   const changeDollar = (event) => {
     setPriceDollar(event.target.value);
@@ -102,6 +136,69 @@ const Beers = () => {
         ""
       )}
       {beersMapped}
+      <Card className="m-4 w-50" bg="success">
+        <Card.Body>
+          <Form className="text-white" onSubmit={submitBeerHandler}>
+            <Row>
+              <Col md={6}>
+                <Form.Group className="mb-3" controlId="beerName">
+                  <Form.Label>Nombre:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    required
+                    value={name}
+                    placeholder="Ingrese el nombre de la creveza"
+                    onChange={changeNameHandler}
+                  />
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group className="mb-3" controlId="beerStyle">
+                  <Form.Label>Estilo:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Ingrese el estilo de la cerveza"
+                    onChange={changeStyleHandler}
+                    value={style}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={6}>
+                <Form.Group className="mb-3" controlId="beerPrice">
+                  <Form.Label>Precio:</Form.Label>
+                  <Form.Control
+                    type="number"
+                    placeholder="Ingrese el precio de la cerveza"
+                    onChange={changePriceHandler}
+                    value={price}
+                  />
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group className="mb-3" controlId="beerAvailable">
+                  <Form.Label>Disponibilidad:</Form.Label>
+                  <Form.Control
+                    type="checkbox"
+                    placeholder="Ingrese la disponibilidad de la cerveza"
+                    onChange={changeAvailable}
+                    value={available}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="justify-content-between"></Row>
+            <Row className="justify-content-end">
+              <Col md={3} className="d-flex justify-content-end">
+                <Button variant="primary" type="submit">
+                  Agregar cerveza
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </Card.Body>
+      </Card>
     </>
   );
 };
